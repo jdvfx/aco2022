b43f7bf0e2895f6d359a9c09c81fc187cf321ea0
Julien Depredurand <julien.depredurand@gmail.com>
2023-01-06 11:27:59 -0800

    cleanup

--- src/main_day8_part2.rs
+++ src/main_day8_part2.rs
@@ -1,11 +1,3 @@
-#![allow(
-    dead_code,
-    unused_variables,
-    unused_assignments,
-    unused_imports,
-    unused_mut
-)]
-
 use std::cmp::max;
 use std::fs;
 
@@ -18,11 +10,10 @@ fn main() {
     // 25512
     // 65332
     // 33549
-    // 35390"
-    //     .to_string();
+    // 35390".to_string();
     // let x = g.split("\n").collect::<Vec<_>>();
 
-    //
+    // parse file into vec of vec<u8>
     let mut trees: Vec<Vec<u8>> = Vec::new();
     let mut viz_trees: Vec<Vec<u8>> = Vec::new();
     for i in x {
@@ -37,60 +28,46 @@ fn main() {
     let dim = viz_trees.len();
 
     let mut max_tree_visible = 0;
-    // top to bottom
+    // loop through all cells
     for y in 1..dim - 1 {
         for x in 1..dim - 1 {
+            // the current tree
             let tallest = trees[y][x];
-
+            //
             let mut left = 0;
             for xx in (0..x).rev() {
-                println!("xx> {:?}",xx);
                 let val = trees[y][xx];
+                left += 1;
                 if val >= tallest {
-                    left += 1;
                     break;
-                } else {
-                    left += 1;
                 }
             }
             let mut right = 0;
             for xx in x + 1..(dim) {
-                println!("xx< {:?}",xx);
-                let val = trees[y][xx];
-                if val >= tallest {
-                    right += 1;
+                right += 1;
+                if trees[y][xx] >= tallest {
                     break;
-                } else {
-                    right += 1;
                 }
             }
             let mut top = 0;
             for yy in (0..y).rev() {
-                println!("yyd {:?}",yy);
-                let val = trees[yy][x];
-                if val >= tallest {
-                    top += 1;
+                top += 1;
+                if trees[yy][x] >= tallest {
                     break;
-                } else {
-                    top += 1;
                 }
             }
             let mut bottom = 0;
             for yy in y + 1..(dim) {
-                println!("yyu {:?}",yy);
-                let val = trees[yy][x];
-                if val >= tallest {
-                    bottom += 1;
+                bottom += 1;
+                if trees[yy][x] >= tallest {
                     break;
-                } else {
-                    bottom += 1;
                 }
             }
             let prod = top * bottom * right * left;
-            println!(
-                "x:{},y:{} t:{} l:{} r:{} t:{} b:{} *{}",
-                x, y, tallest, left, right, top, bottom, prod
-            );
+            // println!(
+            //     "x:{},y:{} t:{} l:{} r:{} t:{} b:{} *{}",
+            //     x, y, tallest, left, right, top, bottom, prod
+            // );
             max_tree_visible = max(max_tree_visible, prod);
         }
     }
